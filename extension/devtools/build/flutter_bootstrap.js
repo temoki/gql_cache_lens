f192df1e347cb23e4f3ce7998c0042b5743db5f2
Tomoki Kobayashi <temoki@gmail.com>
2025-01-11 20:27:38 +0900

    Build

--- extension/devtools/build/flutter_bootstrap.js
+++ extension/devtools/build/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3836552361"
+    serviceWorkerVersion: "1418684516"
   }
 });
